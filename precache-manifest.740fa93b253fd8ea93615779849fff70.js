d20b1231bf38a832a983fcff367c0b0c57332324
Gonzalo Galvez <ggalvezb@live.com>
2020-08-05 22:20:58 -0400

    Updates

---
+++ precache-manifest.740fa93b253fd8ea93615779849fff70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3987430b8c26e89603bfcfcab43bcf9d",
+    "url": "/streaming_twitter/index.html"
+  },
+  {
+    "revision": "2391266f65527b90906f",
+    "url": "/streaming_twitter/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "b3a64b9fe7b09003f157",
+    "url": "/streaming_twitter/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "2391266f65527b90906f",
+    "url": "/streaming_twitter/static/js/2.e4aa6651.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/streaming_twitter/static/js/2.e4aa6651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3a64b9fe7b09003f157",
+    "url": "/streaming_twitter/static/js/main.cc1067ad.chunk.js"
+  },
+  {
+    "revision": "cae60d3de7b70812a45b",
+    "url": "/streaming_twitter/static/js/runtime-main.9d88c591.js"
+  }
+]);
